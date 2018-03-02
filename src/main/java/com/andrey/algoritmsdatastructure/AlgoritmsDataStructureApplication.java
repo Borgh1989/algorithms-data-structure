537fbd4fcace3d353103e023d6e7fe82927b14ae
Andrey Yurkevich <andrey.yurkevich@leverx.com>
2018-03-02 17:28:22 +0300

    init commit

---
+++ src/main/java/com/andrey/algoritmsdatastructure/AlgoritmsDataStructureApplication.java
@@ -0,0 +1,12 @@
+package com.andrey.algoritmsdatastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoritmsDataStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoritmsDataStructureApplication.class, args);
+	}
+}
---
+++ src/test/java/com/andrey/algoritmsdatastructure/AlgoritmsDataStructureApplicationTests.java
@@ -0,0 +1,18 @@
+package com.andrey.algoritmsdatastructure;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlgoritmsDataStructureApplicationTests {
+
+	@Test
+	public void contextLoads() {
+
+
+	}
+
+}
